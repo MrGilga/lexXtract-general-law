41eff561310785e001a6a58066836c6d9aaa210f
DKormann <dominikormann@outlook.com>
2026-05-18 14:22:18 +0200

    build

---
+++ .tmp-tsc/pull_module.js
@@ -0,0 +1,113 @@
+// this script functions 
+// for creating json_content folder
+import { createModule, db } from "../controller/module";
+import { stringify } from "../model/json";
+import { validate } from "../model/pattern";
+import { readFile, writeFile } from "node:fs/promises";
+import { mkdir, rm } from "node:fs/promises";
+const ModulePattern = {
+    id: String,
+    name: String,
+    "sort_order?": Number,
+    "module_type?": String,
+    location: String,
+    "downloaded?": true
+};
+const ModuleListPattern = { modules: [ModulePattern] };
+const [path] = process.argv.slice(2);
+let loop = async () => {
+    let module_list = await readFile(path + "/modules.json").then(d => JSON.parse(d.toString()));
+    const safe = (s) => s.replaceAll(/[^a-z0-9]/gi, "_").toLowerCase();
+    validate(ModuleListPattern, module_list);
+    let mods = await db.get_published();
+    for (const mod of mods) {
+        let modid = (mod.owner + ":" + mod.module + ":" + mod.version);
+        if (module_list.modules.some(l => l.id == modid))
+            continue;
+        db.refresh();
+        let mod_data = await createModule({ owner: mod.owner, name: mod.module }, (c) => { });
+        for (let [catName, cat] of Object.entries(mod_data.extraction.get())) {
+            catName = safe(catName);
+            for (let [subcatName, subcat] of Object.entries(cat)) {
+                subcatName = safe(subcatName);
+                let dir = `${path}/${modid}/en/data/${catName}/${subcatName}`;
+                await mkdir(dir, { recursive: true });
+                for (const [itemName, item] of Object.entries(subcat)) {
+                    let itemParams = {
+                        id: `${modid}_${catName}_${subcatName}_${itemName}`,
+                        name: itemName,
+                        sort_order: 0,
+                        depiction: item.depiction,
+                        taxonomy: { category: catName, subcategory: subcatName }
+                    };
+                    await writeFile(`${dir}/${safe(itemName)}.json`, stringify(itemParams));
+                }
+            }
+        }
+        let taxonomyParams = {
+            taxonomy: {
+                categories: Object.entries(mod_data.extraction.get()).map(([catName, cat]) => ({
+                    id: safe(catName),
+                    name: catName,
+                    sort_order: 0,
+                    description: "",
+                    subcategories: Object.keys(cat).map((subcatName) => ({
+                        id: safe(subcatName),
+                        name: subcatName,
+                        sort_order: 0,
+                        description: "",
+                    }))
+                }))
+            }
+        };
+        await mkdir(`${path}/${modid}/en`, { recursive: true });
+        await writeFile(path + `/${modid}/en/taxonomy.json`, stringify(taxonomyParams));
+        const configParams = {
+            sections: [
+                { name: "title", sort_order: 0 },
+                { name: "content", sort_order: 1 }
+            ],
+            filters: [],
+            category_search_fields: ["name", "description"],
+            data_search_fields: ["name", "depiction"],
+            fields: [
+                {
+                    key: "name",
+                    display_option: "header",
+                    sort_order: 0,
+                    section: "title"
+                },
+                {
+                    key: "depiction",
+                    display_option: "default",
+                    sort_order: 1,
+                    section: "content"
+                }
+            ],
+            form_field_override: [],
+            default_field_option: {
+                key: "any",
+                sort_order: 0,
+                display_option: "hidden",
+                section: "content"
+            }
+        };
+        await writeFile(path + `/${modid}/en/config.json`, stringify(configParams));
+        module_list.modules.push({ id: modid, name: modid, sort_order: 0, module_type: "demo", location: modid, downloaded: true });
+        await writeFile(path + "/modules.json", stringify(module_list));
+    }
+    // console.log(`Pulled ${mods.length} modules`)
+    for (const mod of module_list.modules) {
+        if (mod.downloaded && !mods.some(m => (`${m.owner}:${m.module}:${m.version}`) == mod.id)) {
+            console.log(`Module ${mod.id} is in modules.json but not in database, removing...`);
+            await rm(`${path}/${mod.id}`, { recursive: true });
+            module_list.modules = module_list.modules.filter(m => m.id != mod.id);
+            await writeFile(path + "/modules.json", stringify(module_list));
+        }
+    }
+    setTimeout(() => { loop(); }, 500);
+};
+if (!path || !path.endsWith("json_content"))
+    console.error("Usage: node scripts/pull_module.js <path_to_json_content_folder>");
+else
+    loop();
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/lexXtract-general-law/",
   root: "./",
-  publicDir: false,
+  publicDir: "public",
   build: {
     outDir: "./docs",
     emptyOutDir: true,
